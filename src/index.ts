da78715d21999425b551960afe63b861a8fc3def
Daniel <info@danielvaladas.com>
2026-03-06 17:07:37 -0500

    Removed node_modules and uses ncc to bundle

--- src/index.ts
+++ src/index.ts
@@ -145,7 +145,11 @@ async function run() {
         }
 
     } catch (error) {
-        core.setFailed(error.message);
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        } else {
+            core.setFailed(String(error));
+        }
     }
 }
 
